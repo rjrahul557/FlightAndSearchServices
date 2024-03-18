b578cb19a089b20074ba450abcb7427d19c6faf4
Rahul <rahulrjz600@gmail.com>
2024-03-18 18:01:35 +0530

    setting up sequelize

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const {PORT} = require( './config/serverConfig');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 const  setupAndStartServer = async() => {
     
