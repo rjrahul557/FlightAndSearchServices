d1c207b394776b7e55d994aedccebcd5c81b49cf
Rahul <rahulrjz600@gmail.com>
2024-04-05 04:48:01 +0530

    settingcrud services

--- src/repository/index.js
+++ src/repository/index.js
@@ -2,4 +2,6 @@ module.exports = {
     CityRepository : require('./city_Repository'),
     FlightRepository : require('./flight_Repository'),
     AirplaneRepository : require('./airplane_Repository'),
+    AirportRepository  : require('./airport_Repository'),
+    CrudRepository : require('./crud_Repository'),
 }
